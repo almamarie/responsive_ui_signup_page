c7e7fce578ae9ba3f074ecc1449d80e1a929d631
almamarie <alouismariea97@gmail.com>
2022-01-01 14:04:33 +0000

    wrote the code for the 'or sign up with google' and the text for the terms and conditions and also made the terms and conditions hyperlink to point to 'www.google.com'

--- lib/views/sign_in.dart
+++ lib/views/sign_in.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
+
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SignIn extends StatelessWidget {
   const SignIn({Key? key}) : super(key: key);
@@ -97,12 +99,77 @@ class SignIn extends StatelessWidget {
               SizedBox(height: 16.sp),
               TextButton(
                 onPressed: () {},
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(purpleColor),
+                    foregroundColor: MaterialStateProperty.all(Colors.white),
+                    padding: MaterialStateProperty.all(
+                        EdgeInsets.symmetric(vertical: 14.h))),
                 child: Text(
                   "Create Account",
                   style: TextStyle(
                       fontSize: 18.sp,
                       fontWeight: FontWeight.w700,
                       color: darkTextColor),
+                ),
+              ),
+              SizedBox(height: 16.w),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(child: Divider(height: 6.h, color: Colors.black)),
+                  SizedBox(width: 16.w),
+                  const Text('or sign up with'),
+                  SizedBox(width: 5.w),
+                  Expanded(child: Divider(height: 6.h, color: Colors.black))
+                ],
+              ),
+              SizedBox(
+                height: 16.h,
+              ),
+              TextButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  side:
+                      MaterialStateProperty.all(BorderSide(color: borderColor)),
+                  foregroundColor: MaterialStateProperty.all(darkTextColor),
+                  textStyle: MaterialStateProperty.all(
+                    TextStyle(fontSize: 14.sp, fontWeight: FontWeight.w700),
+                  ),
+                ),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: const [
+                    //Image.asset(''),
+                    Text("Google"),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 16.h,
+              ),
+              RichText(
+                text: TextSpan(
+                  children: [
+                    TextSpan(
+                        style: TextStyle(
+                            color: lightTextColor,
+                            fontWeight: FontWeight.w700,
+                            fontSize: 14.sp),
+                        text: "By signing up to Masterminds, you agree to our"),
+                    TextSpan(
+                        style: TextStyle(
+                            color: purpleColor,
+                            fontSize: 14.sp,
+                            fontWeight: FontWeight.w700),
+                        text: " terms and conditions",
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () async {
+                            String urlString = "www.google.com";
+                            if (await canLaunch(urlString)) {
+                              await launch(urlString);
+                            }
+                          })
+                  ],
                 ),
               )
             ],
